525f7740c21b6c0d0424ac3d028d5f46dc427b5a
fitdticorp4 <fitditcorp@gmail.com>
2022-06-24 00:39:31 +0900

    Project

---
+++ brand_name_generator.py
@@ -0,0 +1,12 @@
+# Brand name Generator Welcome greeting
+print("Welcome!" +" " + "This unique brand name generator creates a unique name for you brand based on user's input")
+
+#First user's input
+First = input("In what city did you grow up? \n")
+
+#Second user' input
+Second = input("What is the name of your pet? \n")
+
+##########################################3
+#This prints out the user's brand brand_name
+print("Your brand name could be: " + First + Second)
