4c9fddbe0a4469c07cebd5f3beb1f71677cea17b
fitdticorp4 <fitditcorp@gmail.com>
2022-06-24 12:15:09 +0900

    Project

---
+++ rem_days.py
@@ -0,0 +1,13 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+age_int = int(age)
+rem_yrs = 100 - age_int
+rem_days = (90 - age_int) * 365
+rem_weeks = rem_yrs * 52
+rem_days = rem_yrs * 365
+rem_mnths = rem_yrs * 12
+
+print(f"You have {rem_days} days, {rem_weeks} weeks, and {rem_mnths} months left. ")
