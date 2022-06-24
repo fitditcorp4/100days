1978e8e71d6444ba2f93bc66792d6f2b3443ccaa
fitdticorp4 <fitditcorp@gmail.com>
2022-06-24 12:20:29 +0900

    Project

--- rem_days.py
+++ rem_days.py
@@ -5,7 +5,7 @@ age = input("What is your current age?")
 #Write your code below this line 👇
 age_int = int(age)
 rem_yrs = 100 - age_int
-rem_days = (90 - age_int) * 365
+rem_days = (100 - age_int) * 365
 rem_weeks = rem_yrs * 52
 rem_days = rem_yrs * 365
 rem_mnths = rem_yrs * 12
